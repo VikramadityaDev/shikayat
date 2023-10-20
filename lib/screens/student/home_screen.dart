cb55fd1f076c4da65453e7c778419df0f0f13ee0
VIKRAMADITYA <vikramadityapratap35@gmail.com>
2023-10-20 23:24:17 +0530

    minor changes

--- lib/screens/student/home_screen.dart
+++ lib/screens/student/home_screen.dart
@@ -1,173 +1,276 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text(
-          "Shikayat",
+    return SafeArea(
+      child: Scaffold(
+        key: scaffoldKey,
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            "Shikayat",
+          ),
+          elevation: 0,
+          backgroundColor: const Color(0xFF10B600),
         ),
-        elevation: 0,
-        backgroundColor: Colors.white,
-      ),
-      drawer: Drawer(
-        backgroundColor: Colors.white,
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            const UserAccountsDrawerHeader(
-              decoration: BoxDecoration(color: Colors.grey),
-              accountName: Text('User Name'),
-              accountEmail: Text('Lpcps@example.com'),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: Colors.white,
-                child: Icon(Icons.person),
+        drawer: Drawer(
+          backgroundColor: Colors.white,
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: Color(0xFF10B600),),
+                accountName: Text('User Name'),
+                accountEmail: Text('Lpcps@example.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(Icons.person),
+                ),
               ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text('Home'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Settings'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
+              ListTile(
+                leading: const Icon(Icons.home),
+                title: const Text('Home'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text('Settings'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  width: 110.0,
-                  height: 75,
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Text("Add Complaint"),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  width: 110.0,
-                  height: 75,
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Text("View"),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  width: 110.0,
-                  height: 75,
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Text("My Complaint"),
-                ),
-              ],
+        body: GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          width: 110.0,
+                          height: 75,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                // Shadow color
+                                spreadRadius: 2,
+                                // Spread radius
+                                blurRadius: 4,
+                                // Blur radius
+                                offset: const Offset(
+                                    0, 2), // Offset from the bottom
+                              ),
+                            ],
+                          ),
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image(
+                                image: AssetImage('assets/add_message.png'),
+                                width: 32,
+                                height: 32,
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                "Add Complaint",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          width: 110.0,
+                          height: 75,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                // Shadow color
+                                spreadRadius: 2,
+                                // Spread radius
+                                blurRadius: 4,
+                                // Blur radius
+                                offset: const Offset(
+                                    0, 2), // Offset from the bottom
+                              ),
+                            ],
+                          ),
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image(
+                                image: AssetImage('assets/view.png'),
+                                width: 32,
+                                height: 32,
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                "View Complaint",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          width: 110.0,
+                          height: 75,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(8),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black.withOpacity(0.2),
+                                // Shadow color
+                                spreadRadius: 2,
+                                // Spread radius
+                                blurRadius: 4,
+                                // Blur radius
+                                offset: const Offset(
+                                    0, 2), // Offset from the bottom
+                              ),
+                            ],
+                          ),
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image(
+                                image: AssetImage('assets/account.png'),
+                                width: 32,
+                                height: 32,
+                              ),
+                              SizedBox(width: 8),
+                              Text(
+                                "My Complaints",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 10.0, top: 10.0),
+                      child: Text(
+                        "Complaint Categories",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 22,
+                          color: Colors.grey[700],
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          height: 250,
+                          width: 170,
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(12)),
+                          child: const Text("Exam related"),
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          height: 250,
+                          width: 170,
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(12)),
+                          child: const Text("Facility related"),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          height: 250,
+                          width: 170,
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(12)),
+                          child: const Text("Document related"),
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          height: 250,
+                          width: 170,
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(12)),
+                          child: const Text("Library related"),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          alignment: Alignment.center,
+                          height: 250,
+                          width: 170,
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(12)),
+                          child: const Text("Canteen related"),
+                        ),
+                        Container(
+                          alignment: Alignment.center,
+                          height: 250,
+                          width: 170,
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(12)),
+                          child: const Text("Lost & Found "),
+                        )
+                      ],
+                    )
+                  ]),
             ),
-            const SizedBox(
-              height: 5,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
-              child: Text(
-                "Categorys",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  height: 250,
-                  width: 170,
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Text("Exam related"),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  height: 250,
-                  width: 170,
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Text("Facility related"),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  height: 250,
-                  width: 170,
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Text("Document related"),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  height: 250,
-                  width: 170,
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Text("Library related"),
-                )
-              ],
-            ),
-            const SizedBox(
-              height: 5,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  height: 250,
-                  width: 170,
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Text("Canteen related"),
-                ),
-                Container(
-                  alignment: Alignment.center,
-                  height: 250,
-                  width: 170,
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(12)),
-                  child: const Text("Lost & Found "),
-                )
-              ],
-            )
-          ]),
+          ),
         ),
       ),
     );
