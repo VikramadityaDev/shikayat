25a633bda1b088abe3ff230db6d013922e032af6
VIKRAMADITYA <vikramadityapratap35@gmail.com>
2023-10-22 13:11:52 +0530

    created constant assets file

---
+++ lib/utils/assets.dart
@@ -0,0 +1,8 @@
+class MyAssets {
+  static const String examRelated = 'assets/icons/exam_related.json';
+  static const String facilityRelated = 'assets/icons/facility_related.json';
+  static const String documentRelated = 'assets/icons/document_related.json';
+  static const String libraryRelated = 'assets/icons/library_related.json';
+  static const String canteenRelated = 'assets/icons/canteen_related.json';
+  static const String lostAndFound = 'assets/icons/lost&found.json';
+}
