442e07f58581f79841c0d483dba8f682ea59fc8a
VIKRAMADITYA <vikramadityapratap35@gmail.com>
2023-10-24 16:12:24 +0530

    minor changes

--- lib/app.dart
+++ lib/app.dart
@@ -1,137 +1,144 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:shikayat/auth/login_page.dart';
+// import 'package:shikayat/auth/register_page.dart';
 
 class MainPage extends StatelessWidget {
   const MainPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xffffffff),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            const Align(
-              alignment: Alignment(0.0, 0.4),
-              child: Column(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xffffffff),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              const Align(
+                alignment: Alignment(0.0, 0.4),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Align(
+                      alignment: Alignment(0.1, -0.1),
+                      child:
+                          Image(
+                        image: AssetImage("assets/shikayat_logo.png"),
+                        height: 170,
+                        width: 170,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+                          child: Text(
+                            "S H I K A Y A T",
+                            textAlign: TextAlign.start,
+                            overflow: TextOverflow.clip,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontStyle: FontStyle.normal,
+                              fontSize: 28,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisSize: MainAxisSize.max,
                 children: [
-                  Align(
-                    alignment: Alignment(0.1, -0.1),
-                    child:
-                        Image(
-                      image: AssetImage("assets/shikayat_logo.png"),
-                      height: 170,
-                      width: 170,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                        child: Text(
-                          "S H I K A Y A T",
-                          textAlign: TextAlign.start,
-                          overflow: TextOverflow.clip,
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontStyle: FontStyle.normal,
-                            fontSize: 28,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                MaterialButton(
-                  onPressed: () {},
-                  color: const Color(0xFF10B600),
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(22.0),
-                  ),
-                  padding: const EdgeInsets.all(16),
-                  textColor: const Color(0xffffffff),
-                  height: 50,
-                  minWidth: MediaQuery.of(context).size.width,
-                  child: const Text(
-                    "SIGN UP",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                      fontStyle: FontStyle.normal,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                  child: MaterialButton(
-                    onPressed: () {},
-                    color: const Color(0xFF000000),
+                  MaterialButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed('/register');
+                    },
+                    color: const Color(0xFF10B600),
                     elevation: 0,
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(22.0),
+                      borderRadius: BorderRadius.circular(6.0),
                     ),
                     padding: const EdgeInsets.all(16),
                     textColor: const Color(0xffffffff),
                     height: 50,
                     minWidth: MediaQuery.of(context).size.width,
-                    child: const Text(
-                      "SIGN IN",
-                      style: TextStyle(
+                    child: Text(
+                      "SIGN UP",
+                      style: GoogleFonts.lato(
                         fontSize: 16,
                         fontWeight: FontWeight.w700,
                         fontStyle: FontStyle.normal,
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
-                  child: MaterialButton(
-                    onPressed: () {},
-                    color: const Color(0xffffffff),
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(22.0),
-                      side:
-                          const BorderSide(color: Color(0xffffffff), width: 1),
-                    ),
-                    padding: const EdgeInsets.all(16),
-                    textColor: const Color(0xff000000),
-                    height: 40,
-                    minWidth: 140,
-                    child: const Text(
-                      "Are you an Admin? Click Here",
-                      style: TextStyle(
-                        fontSize: 17,
-                        fontWeight: FontWeight.w700,
-                        fontStyle: FontStyle.italic,
-                        decoration: TextDecoration.underline,
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                    child: MaterialButton(
+                      onPressed: () => Navigator.of(context).pushNamed('/login'),
+                      color: const Color(0xFF000000),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6.0),
+                      ),
+                      padding: const EdgeInsets.all(16),
+                      textColor: const Color(0xffffffff),
+                      height: 50,
+                      minWidth: MediaQuery.of(context).size.width,
+                      child: Text(
+                        "SIGN IN",
+                        style: GoogleFonts.lato(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.normal,
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 25, 0, 0),
+                    child: MaterialButton(
+                      onPressed: () => Navigator.of(context).pushNamed('/adminLogin'),
+                      color: const Color(0xffffffff),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(22.0),
+                        side:
+                            const BorderSide(color: Color(0xffffffff), width: 1),
+                      ),
+                      padding: const EdgeInsets.all(16),
+                      textColor: const Color(0xff000000),
+                      height: 40,
+                      minWidth: 140,
+                      child: Text(
+                        "Are you an Admin? Click Here",
+                        style: GoogleFonts.lato(
+                          fontSize: 17,
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.normal,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
