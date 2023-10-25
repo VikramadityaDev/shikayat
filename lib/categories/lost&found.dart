c59e0f8cf42488b47738bc7bb609e7df88c65998
VIKRAMADITYA <vikramadityapratap35@gmail.com>
2023-10-25 13:43:27 +0530

    minor changes

--- lib/categories/lost&found.dart
+++ lib/categories/lost&found.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:shikayat/utils/assets.dart';
 
@@ -25,34 +26,39 @@ class LostAndFound extends StatelessWidget {
               const SizedBox(
                 height: 10,
               ),
-              const Text(
+              Text(
                 "Complaints related to lost and found involve items lost by students on the college campus, and students can register these complaints to help them to recover their belongings.",
-                maxLines: 5,
+                maxLines: 6,
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: GoogleFonts.lato(
                   fontSize: 20,
                 ),
               ),
               const SizedBox(
                 height: 50,
               ),
-              MaterialButton(
-                onPressed: () {},
-                color: const Color(0xFF000000),
-                elevation: 0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(22.0),
-                ),
-                padding: const EdgeInsets.all(16),
-                textColor: const Color(0xffffffff),
-                height: 50,
-                minWidth: MediaQuery.of(context).size.width,
-                child: const Text(
-                  "View All Lost&Found Complaints",
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w700,
-                    fontStyle: FontStyle.normal,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 1),
+                child: MaterialButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/lostAndFoundRelated');
+                  },
+                  color: const Color(0xFF000000),
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(6.0),
+                  ),
+                  padding: const EdgeInsets.all(16),
+                  textColor: const Color(0xffffffff),
+                  height: 50,
+                  minWidth: MediaQuery.of(context).size.width,
+                  child: Text(
+                    "View All Lost & Found Complaints",
+                    style: GoogleFonts.lato(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.normal,
+                    ),
                   ),
                 ),
               ),
